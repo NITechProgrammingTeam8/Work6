c52012fbcd123944db01f561108c2560227e3bad
ash1098 <56241628+ash1098@users.noreply.github.com>
2020-01-06 15:59:52 +0900

    アオヤマプログラム完成

---
+++ task5-7/Block World Planning/Assets/Script/SelectOnList.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SelectOnList : MonoBehaviour
+{
+    public GameObject master;
+    public GameObject rm;
+
+    void Awake()
+    {
+        master = GameObject.Find("Master");
+        rm = GameObject.Find("ButtonRm");
+    }
+
+    public void Select()
+    {
+        master.GetComponent<StateGetter>().FocusOutline(GetComponent<Manager>().planObj);
+        rm.GetComponent<Destroyer>().target = this.gameObject;
+    }
+}
---
+++ task5-7/Block World Planning/Assets/Script/StateGetter.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StateGetter : MonoBehaviour {
+
+    public GameObject target;
+
+    public void FocusOutline (GameObject newObj) {
+        if (target != null) {
+            target.GetComponent<Outline> ().enabled = false;
+        }
+        if (newObj != null) {
+            newObj.GetComponent<Outline> ().enabled = true;
+        }
+        target = newObj;
+    }
+
+    public Text status;
+
+    void Update () {
+        if (target != null) {
+            status.text = "[" + target.name + "]";
+
+            foreach (GameObject obj in target.GetComponent<CollisionGetter> ().colList) {
+                if (obj != null) {  // Removeした物体を除外
+                    status.text += "\n" + obj.name;
+                }
+            }
+        }
+    }
+}
